5673f10d087b2cb15e08e56f8ae18fad203d7596
Xonnel50 <lennoxbailey@yahoo.com>
2022-11-29 21:36:11 -0500

    First commit

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+  }
+  required_version = "~> 1.0"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
